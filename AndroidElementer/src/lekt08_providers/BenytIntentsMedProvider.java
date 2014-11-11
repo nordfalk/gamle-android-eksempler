0120ad02c01354fe41569bf85b0d325dac6bb558
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2014-11-11 16:59:42 +0100

    provider til at modtage og sende filer. Bruges til:               - at modtage billeddata fra kamera-intents               - at sende data med et intent via f.eks gmail

---
+++ AndroidElementer/src/lekt08_providers/BenytIntentsMedProvider.java
@@ -0,0 +1,99 @@
+package lekt08_providers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.io.File;
+
+/**
+ * Demonstrerer hvordan man benytter en ccontent providerintents til at vælg en
+ * kontaktperson, et billede eller tage et billede med kameraet
+ *
+ * @author Jacob Nordfalk
+ */
+public class BenytIntentsMedProvider extends Activity implements OnClickListener {
+
+  Button tagBillede, sendBillede;
+  TextView resultatTextView;
+  LinearLayout resultatHolder;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    TableLayout tl = new TableLayout(this);
+    tagBillede = new Button(this);
+    tagBillede.setText("Tag billede med kameraet");
+    tagBillede.setOnClickListener(this);
+    tl.addView(tagBillede);
+
+    sendBillede = new Button(this);
+    sendBillede.setText("Send billede som vedhæftning");
+    sendBillede.setOnClickListener(this);
+    tl.addView(sendBillede);
+
+    resultatTextView = new TextView(this);
+    tl.addView(resultatTextView);
+
+    resultatHolder = new LinearLayout(this);
+    tl.addView(resultatHolder);
+
+    ScrollView sv = new ScrollView(this);
+    sv.addView(tl);
+    setContentView(sv);
+  }
+
+  public void onClick(View hvadBlevDerKlikketPå) {
+      if (hvadBlevDerKlikketPå == tagBillede) {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Vi vil have billedet gemt i vores content provider:
+        File fil = new File(Environment.getExternalStorageDirectory(),"billede.jpg");
+        //i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(fil));
+        i.putExtra(MediaStore.EXTRA_OUTPUT, KamerabilledeProvider.URI);
+        startActivityForResult(i, 42);
+
+      } else {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"jacob.nordfalk@gmail.com"});
+        i.putExtra(Intent.EXTRA_CC, new String[]{"jacob.nordfalk@gmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Et billede fra AndroidElementer");
+        i.putExtra(Intent.EXTRA_TEXT, "Her er et billede");
+        i.putExtra(Intent.EXTRA_STREAM, KamerabilledeProvider.URI);
+
+        startActivity(Intent.createChooser(i, "Send e-post..."));
+      }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent resIntent) {
+    resultatTextView.setText(requestCode + " gav resultat " + resultCode + " og data:\n" + resIntent);
+    System.out.println(requestCode + " gav resultat " + resultCode + " med data=" + resIntent);
+
+    resultatHolder.removeAllViews();
+
+    if (resultCode == Activity.RESULT_OK) {
+      ImageView iv = new ImageView(this);
+      iv.setImageURI(KamerabilledeProvider.URI);
+      resultatHolder.addView(iv);
+      iv = new ImageView(this);
+      iv.setImageResource(android.R.drawable.btn_star);
+      resultatHolder.addView(iv);
+    }
+    ImageView iv = new ImageView(this);
+    iv.setImageResource(android.R.drawable.ic_dialog_email);
+    resultatHolder.addView(iv);
+
+  }
+}
---
+++ AndroidElementer/src/lekt08_providers/KamerabilledeProvider.java
@@ -0,0 +1,103 @@
+package lekt08_providers;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.ProviderInfo;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import lekt04_arkitektur.MinApp;
+
+/**
+ * Taget fra:
+ * http://stackoverflow.com/questions/10042695/how-to-get-camera-result-as-a-uri-in-data-folder
+ * http://dharmendra4android.blogspot.in/2012/04/save-captured-image-to-applications.html
+ */
+public class KamerabilledeProvider extends ContentProvider {
+  public static final Uri URI = Uri.parse("content://dk.nordfalk.android.elementer.KamerabilledeProvider/1");
+  public static final String FILNAVN = "billede.jpg";
+
+  @Override
+  public ParcelFileDescriptor openFile(Uri uri, String mode) throws FileNotFoundException {
+    log("openFile(\n" + uri + "\nmode=" + mode);
+    try {
+      File f = new File(getContext().getFilesDir(), FILNAVN);
+      if (mode.equals("w")) {
+        f.delete();
+        f.createNewFile();
+      }
+      return ParcelFileDescriptor.open(f, ParcelFileDescriptor.MODE_READ_WRITE);
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new FileNotFoundException(uri.getPath());
+    }
+  }
+
+  private void log(final String tekst) {
+    Log.d("KamerabilledeProvider", tekst);
+    MinApp.forgrundstråd.post(new Runnable() {
+      @Override
+      public void run() {
+        Toast.makeText(getContext(), "KamerabilledeProvider +"+tekst, Toast.LENGTH_LONG).show();
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreate() {
+    log("onCreate");
+    return true;
+  }
+
+  @Override
+  public String getType(Uri uri) {
+    log("getType("+uri);
+    return "image/jpg";
+  }
+
+
+  @Override
+  public void attachInfo(Context context, ProviderInfo info) {
+    super.attachInfo(context, info);
+    log("attachInfo("+info.toString());
+
+    // Sanity check our security
+    if (info.exported) {
+      //throw new SecurityException("Provider must not be exported");
+    }
+    if (!info.grantUriPermissions) {
+      //throw new SecurityException("Provider must grant uri permissions");
+    }
+  }
+
+
+  @Override
+  public int delete(Uri uri, String selection, String[] selectionArgs) {
+    return 0;
+  }
+
+  @Override
+  public Uri insert(Uri uri, ContentValues values) {
+    return null;
+  }
+
+  @Override
+  public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                      String sortOrder) {
+    log("query( " + uri);
+    return null;
+  }
+
+  @Override
+  public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    return 0;
+  }
+}
